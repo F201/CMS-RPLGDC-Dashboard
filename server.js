7ec3f226c3df4da0811be5af4ce7618242d92dc8
Aji P Santoso <ajisantoso12@gmail.com>
2020-02-13 11:18:56 +0700

    add history routing

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+var history = require('connect-history-api-fallback');
 
 const app = express();
+
+app.use(history({
+  verbose: true
+}));
 
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
